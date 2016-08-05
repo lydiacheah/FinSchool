368033df2ed4ed91427cbd141d801489c6e2c387
Wong Tze Hsiung <wotzhs@gmail.com>
2016-08-05 11:31:24 +0800

    applied migration for transaction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805031337) do
+ActiveRecord::Schema.define(version: 20160805032659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 20160805031337) do
     t.boolean  "active",                                  default: true
     t.datetime "created_at",                                             null: false
     t.datetime "updated_at",                                             null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "game_id",                              null: false
+    t.integer  "user_id",                              null: false
+    t.decimal  "start_amount", precision: 8, scale: 2, null: false
+    t.decimal  "end_amount",   precision: 8, scale: 2, null: false
+    t.integer  "months",                               null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
   create_table "users", force: :cascade do |t|
