e69ccd439cdd877293975ba458307d5cca5fb86b
Wong Tze Hsiung <wotzhs@gmail.com>
2016-08-05 11:25:51 +0800

    created profile model & applied activerecord association between user & profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_many :authentications, :dependent => :destroy
+  has_many :profiles
 
   def self.create_with_auth_and_hash(authentication,auth_hash)
     create! do |u|
