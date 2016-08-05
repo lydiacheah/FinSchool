738bd0434073145f32d1cfa01b7996bcc017a117
Wong Tze Hsiung <wotzhs@gmail.com>
2016-08-05 11:34:26 +0800

    applied migration for games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805032659) do
+ActiveRecord::Schema.define(version: 20160805033143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20160805032659) do
     t.string   "token"
     t.string   "provider"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
