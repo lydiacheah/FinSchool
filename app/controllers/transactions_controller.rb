0da98d030d8d0e0e00844fc8e230ffff8c75d1b8
imranfazley <imranfazley@gmail.com>
2016-08-08 19:42:38 +0800

    insert variables for progress bar and ROI figures

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,33 +6,39 @@ class TransactionsController < ApplicationController
 			@msg = "You have withdrawn your fixed deposit placement"
 		end
 	end
+
 	def show
 	end
-	def index
-		@user_transaction = Transaction.last
-		
-		@for_chart = Transaction.all
 
-		## For the current user, determine the profile ID
+	def index
+		# @user_transaction = Transaction.last
+		# @for_chart = Transaction.all
+
+		# For the current user, determine the profile ID
 		@profile = Profile.find_by(user_id: current_user.id)
-		## From the profile ID, list out all the transactions
+	
+		# From the profile ID, list out all the transactions
 		@user_list_transaction = @profile.transactions
 
-
-		##                First Row                ##
-		## Calculation of variables to be charted:
-
-		@financial_goal = 500000
-		@current_balance = 12000
+		# Calculating progress towards financial goals
+		@financial_goal = @profile.financial_goal
+		@current_balance = @profile.current_balance
+		@financial_progress = (@current_balance/@financial_goal)*100
 
 		## Fixed Deposit ROI values
-		@fixed_deposit_roi = 0.56
+		@fd_investments = @user_list_transaction.where(game_id: 1).sum(:start_amount)
+		@fd_returns = @user_list_transaction.where(game_id: 1).sum(:end_amount)
+		@fixed_deposit_roi = ((@fd_returns-@fd_investments)/@fd_investments) * 100
 
 		## Unit Trust ROI values
-		@unit_trust_roi = 0.23
+		@ut_investments = @user_list_transaction.where(game_id: 2).sum(:start_amount)
+		@ut_returns = @user_list_transaction.where(game_id: 2).sum(:end_amount)
+		@unit_trust_roi = ((@ut_returns-@ut_investments)/@ut_investments) * 100
 
 		## Stock Market ROI values
-		@stock_market_roi = 0.45
+		@sm_investments = @user_list_transaction.where(game_id: 3).sum(:start_amount)
+		@sm_returns = @user_list_transaction.where(game_id: 3).sum(:end_amount)
+		@stock_market_roi = ((@sm_returns-@sm_investments)/@sm_investments) * 100
 
 		##                Second Row                ##
 		## Calculation of variables to be charted:
