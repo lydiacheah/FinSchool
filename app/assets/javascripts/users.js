872c484c2fc8ca1d2030229eee75d2fcf44f72dd
Lydia Cheah <cheahlizuan@gmail.com>
2016-08-08 14:46:01 +0800

    improve UX on edit user page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,18 +1,10 @@
 $(document).ready(function() {
 	$('.sign-up-questions').fadeIn(900);
-	
-	$("#edit-user-form").on('keyup keypress', function(e) {
-	  var keyCode = e.keyCode || e.which;
-	  if (keyCode === 13) { 
-	    e.preventDefault();
-	    return false;
-	  }
-	});
 
-	$("#nickname-input").keyup(function(event){
+	$("#nickname-input").keypress(function(event){
     if(event.keyCode == 13){
-    	event.preventDefault();
       $("#sign-up-continue").click();
+    	return false;
     }
 	});
 
